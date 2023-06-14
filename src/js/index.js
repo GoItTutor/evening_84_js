28f95fdb37f0b3505ee9776cee61f4d4b6449bf7
GoItTutor <sheva09a@gmail.com>
2023-06-14 21:30:36 +0200

    module-08-lesson-01

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 console.log('Hello from index.js');
+
+const arr = [1, 2, 3];
+
+const [a, b, c] = arr;
+
+console.log(a, b, c);
+
+const isAdmin = true ? true : false;
+
+const arrowFunc = () => {};
+
+const templateString = `${isAdmin}`;
