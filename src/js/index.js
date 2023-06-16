a7b6f023f12a6e49c2acf0386024a4096a63e18d
GoItTutor <sheva09a@gmail.com>
2023-06-16 20:30:34 +0200

    module-08-lesson-02

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,76 @@
 'use strict';
+
+// import Obj, { add } from './math';
+
+// console.log(Obj);
+// console.log(add);
+
+// import * as MathModule from './math';
+
+// console.log(MathModule);
+
+// const user = {
+//   firstname: 'Pavel',
+//   lastname: 'Nekrasov',
+//   age: 30,
+//   gender: 'male',
+
+//   showName() {
+//     console.log(this.firstname);
+//   },
+
+//   city: null,
+//   occupation: undefined,
+//   family: {
+//     mom: 'Lidiya',
+//   },
+//   hobbies: [],
+// };
+
+// const userJSON = JSON.stringify(user);
+
+// const userJS = JSON.parse('"Hello!"');
+
+// console.log(userJSON);
+// console.log(userJS);
+
+// const newUser = JSON.parse(JSON.stringify(user));
+
+// console.log(newUser);
+// console.log(newUser === user);
+
+// console.log(`First`);
+
+// const user = '"Hello!"';
+
+// let userJSON = null;
+
+// try {
+//   userJSON = JSON.parse(user);
+
+//   console.log(`Finish of try block`);
+// } catch (err) {
+//   console.log(err);
+// } finally {
+//   console.log(`Finish`);
+// }
+
+// console.log(userJSON);
+
+// console.log(`Second`);
+
+const user = {
+  firstname: 'Tomas',
+  lastname: 'Shelby',
+  age: 45,
+};
+
+localStorage.setItem('user', JSON.stringify(user));
+
+localStorage.setItem('name', JSON.stringify('Andrii'));
+
+const value = JSON.parse(localStorage.getItem('user'));
+
+// localStorage.clear();
+
+localStorage.removeItem('user');
--- src/js/math.js
+++ src/js/math.js
@@ -1,17 +1,21 @@
 'use strict';
 
-const add = (a, b) => {
+export const add = (a, b) => {
   return a + b;
 };
 
-const sub = (a, b) => {
+export const sub = (a, b) => {
   return a - b;
 };
 
-const division = (a, b) => {
+export const division = (a, b) => {
   return a / b;
 };
 
-const multi = (a, b) => {
+export const multi = (a, b) => {
   return a * b;
 };
+
+export default { a: 5, b: 10 };
+
+console.log(`Hi!`);
