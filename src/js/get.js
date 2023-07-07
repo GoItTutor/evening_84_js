6b86c6585a08fa283fdbcabfd302ae91f12d8350
GoItTutor <sheva09a@gmail.com>
2023-07-07 20:41:05 +0200

    module-11-lesson-01

--- src/js/get.js
+++ src/js/get.js
@@ -1,8 +1,24 @@
 'use strict';
 
+import axios from 'axios';
+
 /*
  * Method (GET).
  */
 
 const BASE_URL = 'http://localhost:3000';
-const API_KEY = 'LxvKVGJqiSe6NcEVZOaLXC-f2JIIWZaq_o0WrF8mwJc';
+
+// const getComments = () =>
+//   fetch(`${BASE_URL}/comments`, { method: 'GET' }).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+
+//     return response.json();
+//   });
+
+const getComments = () => axios.get(`${BASE_URL}/comments`);
+
+getComments()
+  .then(response => console.log(response.data))
+  .catch(console.warn);
--- src/js/put-patch.js
+++ src/js/put-patch.js
@@ -1,17 +1,35 @@
 'use strict';
 
+const { default: axios } = require('axios');
+
 /*
  * Method(PUT/PATCH). Headers ("Content-Type": "application/json"). Body.
  */
 
 const BASE_URL = 'http://localhost:3000';
 
-// const updateUserById = (userId, infoForUpdate) => {}
+// const updateUserById = (userId, infoForUpdate) =>
+//   fetch(`${BASE_URL}/users/${userId}`, {
+//     method: 'PATCH',
+//     body: JSON.stringify(infoForUpdate),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   }).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
 
-// updateUserById(15, { name: 'Oleksii Repin', email: 'test@gmail.com' })
-//   .then(data => {
-//     console.log(data);
-//   })
-//   .catch(err => {
-//     console.log(err);
+//     return response.json();
 //   });
+
+const updateUserById = (userId, infoForUpdate) =>
+  axios.patch(`${BASE_URL}/users/${userId}`, infoForUpdate);
+
+updateUserById(1, { address: {} })
+  .then(({ data }) => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
